0bee9580de6e2907b9c9fdf8263cda657b4532b3
Thgiliwt <83489530+Thgiliwt@users.noreply.github.com>
2021-10-12 23:50:45 +1100

    updates

--- app.py
+++ app.py
@@ -2,6 +2,16 @@
 import numpy as np
 from flask import Flask, request, render_template
 import pickle
+from sklearn.metrics import classification_report
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV,StratifiedKFold
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import  precision_recall_curve, roc_auc_score, confusion_matrix, accuracy_score, recall_score, precision_score, f1_score,auc, roc_curve, plot_confusion_matrix
+
+
 
 
 # TO LOAD THE MODEL AND THE SCALER!
